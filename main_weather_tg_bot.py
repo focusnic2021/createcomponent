56ee55a61423dbcbc30142dbfa10c0f719e72324
Albert Ibragimov <82354553+focusnic2021@users.noreply.github.com>
2022-07-29 12:49:57 +0500

    обращение к БД

--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -4,6 +4,7 @@ https://www.youtube.com/watch?v=fa1FUW1jLAE
 """
 import requests
 import datetime
+import sqlite3
 from config import open_weather_token, tg_bot_token
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
@@ -27,14 +28,70 @@ def protocol(event_mesaage):
     # ...def protocol(event_mesaage)
 
 
+def db_insert_user(data):
+    """Insert record about new user - his id, first_name, last_name, datetime"""
+    try:
+        # запись в базу данных:
+        connect = sqlite3.connect('users_query.db')
+        cursor = connect.cursor()
+        sql = f"SELECT dt FROM users WHERE id = {data[0]}"
+        cursor.execute(sql)
+        user_is_exist = cursor.fetchone()
+        if user_is_exist is None:
+            # такого пользователя нет - вставить запись о нем:
+            sql = "INSERT INTO users(id, first_name, last_name, dt) VALUES(?,?,?,?);"
+            print(sql)
+            cursor.execute(sql, data)
+            connect.commit()
+        else:
+            print(f"Знаем такого, был здесь {user_is_exist}")
+    except Exception as ex:
+        print('Ошибка записи в БД', ex)
+    # ...def db_insert_user(sql)
+
+
+def db_insert_event(data):
+    """Insert record about user's query  - user_id, message_text, result, dt"""
+    try:
+        # запись в базу данных:
+        connect = sqlite3.connect('users_query.db')
+        cursor = connect.cursor()
+        sql = "INSERT INTO events(user_id, message_text, result, dt) VALUES(?,?,?,?);"
+        cursor.execute(sql, data)
+        connect.commit()
+        # запрос предыдущих запросов этого пользователя:
+        old_records = [] # список для заполнения
+        # sql = f"SELECT DISTINCT message_text FROM events WHERE user_id = {data[0]} AND result <> 'city not found' ORDER BY dt DESC LIMIT 3"
+        sql = f"""SELECT DISTINCT message_text FROM
+            (SELECT message_text FROM events WHERE user_id = {data[0]} AND result <> 'city not found'
+            ORDER BY dt DESC LIMIT 10)
+        LIMIT 3"""
+        # print(sql)
+        cursor.execute(sql)
+        [old_records.append(elem[0]) for elem in cursor.fetchmany(3)]
+        return old_records
+    except Exception as ex:
+        print('Ошибка записи в БД', ex)
+        return ['error!', ex]
+    # ...def db_insert_event(data)
+
+
 @dp.message_handler(commands=['start','help'])
 async def start_command(message: types.Message):
-    # внести сообщение о событии в протокол:
+    # внести сообщение о событии в протокол и БД:
     event_msg = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')};" \
                 f"{message.from_user.id};" \
                 f"{message.from_user.first_name} {message.from_user.last_name};" \
                 f"command={message.text};\n"
+    # запись в лог-файл:
     protocol(event_msg)
+    # запись в БД:
+    db_insert_user((
+        message.from_user.id,
+        message.from_user.first_name,
+        message.from_user.last_name,
+        datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    ))
 
     # приветствовать пользователя:
     await message.reply('Погода на данный момент в определенном городе.\nПример запроса:\nУчалы\nkazan\nalanya')
@@ -66,37 +123,39 @@ async def get_weather(message: types.Message):
             # время восхода и заката на сегодня в этом городе - с поправкой на часовой пояс:
             sunrise = datetime.datetime.fromtimestamp(data['sys']['sunrise']+data['timezone']).strftime('%H:%M')
             sunset  = datetime.datetime.fromtimestamp(data['sys']['sunset']+data['timezone']).strftime('%H:%M')
-            print(f"timezone={data['timezone']}")
-            print(f"sunrise={sunrise}, sunset={sunset}")
+            # print(f"timezone={data['timezone']}\n sunrise={sunrise}, sunset={sunset}")
             length_of_day = datetime.datetime.fromtimestamp(data['sys']['sunset']) - datetime.datetime.fromtimestamp(data['sys']['sunrise'])
             # данные о погоде:
             temp     = data['main']['temp']             # температура
-            pressure = data['main']['pressure']         # давление
+            pressure = str(int(data['main']['pressure'])-300)         # давление # pressure = data['main']['pressure']         # давление
             humidity = data['main']['humidity']         # влажность
             wind_deg = data['wind']['deg']      # направление ветра
             # wind_kompas =''
             if wind_deg < 31:
-                wind_kompas = 'Северный'
+                wind_kompas = 'С' # wind_kompas = 'Северный'
             elif wind_deg > 30 and wind_deg < 61:
-                wind_kompas = 'Северо-Восточный'
+                wind_kompas = 'СВ' # wind_kompas = 'Северо-Восточный'
             elif wind_deg > 60 and wind_deg < 121:
-                wind_kompas = 'Восточный'
+                wind_kompas = 'В' # wind_kompas = 'Восточный'
             elif wind_deg > 120 and wind_deg < 151:
-                wind_kompas = 'Юго-Восточный'
+                wind_kompas = 'ЮВ' # wind_kompas = 'Юго-Восточный'
             elif wind_deg > 150 and wind_deg < 211:
-                wind_kompas = 'Южный'
+                wind_kompas = 'Ю' # wind_kompas = 'Южный'
             elif wind_deg > 210 and wind_deg < 241:
-                wind_kompas = 'Юго-Западный'
+                wind_kompas = 'ЮЗ' # wind_kompas = 'Юго-Западный'
             elif wind_deg > 240 and wind_deg < 301:
-                wind_kompas = 'Западный'
+                wind_kompas = 'З' # wind_kompas = 'Западный'
             elif wind_deg > 300 and wind_deg < 331:
-                wind_kompas = 'Северо-Западный'
+                wind_kompas = 'СЗ' # wind_kompas = 'Северо-Западный'
             else:
-                wind_kompas = 'Северный'
+                wind_kompas = 'С' # wind_kompas = 'Северный'
             wind_speed = data['wind']['speed']  # скорость ветра
-            await message.reply(f"Погода в <b>{city}</b> - {country} (координаты: {lat},{lon})\n\t"
-                f"по состоянию на {dt}\n\t"
-                f"Температура: {temp} °С\n\tДавление: {pressure} мм.рт.ст\n\tВлажность: {humidity} %\n\t"
+            await message.reply(f"Погода в <b>{city}</b> - {country} \n"
+                f"(коорд: {lat},{lon})\n"
+                f"по состоянию на {dt}\n"
+                f"Температура: {temp} °С\n"
+                f"Давление: {pressure} мм.рт.ст\n"
+                f"Влажность: {humidity} %\n"
                 f"Солнце:\n\tВосход - {sunrise}, Закат - {sunset}, Световой день - {length_of_day}\n\t"
                 f"Ветер: {wind_kompas} ({wind_deg}°),  {wind_speed} м/с")
 
@@ -110,9 +169,21 @@ async def get_weather(message: types.Message):
                     f"message.text={message.text};" \
                     f"result={response_result}\n"
         protocol(event_msg)
+        # запись в БД - и возвращение старых запросов этого пользователя:
+        old_queries = db_insert_event((
+            message.from_user.id,
+            message.text,
+            response_result[0:30],
+            datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        ))
+        # print(old_queries)
+        # сформировать keyboard и представить пользователю
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*old_queries)
+        await message.answer("Еще запрос?", reply_markup=keyboard)
 
     except Exception as ex:
-        # занести в протокол:
+        # Ошибку - тоже занести в протокол:
         event_msg = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')};" \
                     f"{message.from_user.id};" \
                     f"{message.from_user.first_name} {message.from_user.last_name};" \
